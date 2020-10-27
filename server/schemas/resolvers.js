304aae6ae6c2d073153b2d54db81aebde8b0b101
Jacob Daughtry <akutozo@gmail.com>
2020-10-26 23:25:06 -0500

    Back-End build out

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -34,6 +34,30 @@ const resolvers = {
             const token = signToken(user);
         return { token, user };
         },
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,12 +9,43 @@ type Query {
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User
+    removeBook(bookId: ID!): User
 }
 
 type User {
     _id: ID
     username: String
     email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Author {
+    _id: ID
+    description: String
+    bookId: String
+    image: String
+    links: String
+}
+
+type Book { 
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+
 }
 
 type Auth {
